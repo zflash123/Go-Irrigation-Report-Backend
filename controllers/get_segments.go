2c3ea529487b2004a52f7333a1a6f13da576a433
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-04 12:50:45 +0700

    Create CloseSegments struct with properties such as ID, Name, and Geojson

--- controllers/get_segments.go
+++ controllers/get_segments.go
@@ -2,9 +2,16 @@ package controllers
 
 import (
 	"net/http"
+	"github.com/google/uuid"
 )
-
+type CloseSegments struct {
+	ID				uuid.UUID
+	Name			string
+	Geojson		interface{}
+}
 func GetCloseSegments(w http.ResponseWriter, r *http.Request) {
 	latitude:= r.URL.Query().Get("lat")
 	longitude:= r.URL.Query().Get("long")
+
+
 }
