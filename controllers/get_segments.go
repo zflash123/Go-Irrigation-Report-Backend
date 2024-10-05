b0832458e8ed967615ff1a419d411e4ff6d4ad43
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-05 11:02:41 +0700

    Change the Geojson property of CloseSegments struct data type to string in the get_segments.go.

--- controllers/get_segments.go
+++ controllers/get_segments.go
@@ -10,7 +10,7 @@ import (
 type CloseSegments struct {
 	ID				uuid.UUID
 	Name			string
-	Geojson		interface{}
+	Geojson		string
 }
 func GetCloseSegments(w http.ResponseWriter, r *http.Request) {
 	latitude:= r.URL.Query().Get("lat")
