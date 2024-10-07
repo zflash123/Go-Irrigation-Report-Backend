4957efec7b9a28336b16e672dcaacb0d6604c836
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-07 15:59:29 +0700

    Add the columns created_at and done_at from the report_list table in select

--- controllers/reports.go
+++ controllers/reports.go
@@ -17,7 +17,7 @@ func GetReportById(w http.ResponseWriter, r *http.Request){
 	report_id := mux.Vars(r)["id"]
 	var reports []Reports
 	queryReports:= models.Db.Table("report.report_list").
-	Select("report.report_list.id").
+	Select("report.report_list.id", "report.report_list.created_at", "report.report_list.done_at").
 	Scan(&reports)
 
 	if queryReports.Error != nil {
