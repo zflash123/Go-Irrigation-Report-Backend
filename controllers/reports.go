d5d64b72cbec091cab1f1d8ce3e5bbe7ce5a0141
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-07 17:00:08 +0700

    Add Joins to the report.status table. Then, add the name columns from the report.status table in the Select statement.

--- controllers/reports.go
+++ controllers/reports.go
@@ -22,7 +22,8 @@ func GetReportById(w http.ResponseWriter, r *http.Request){
 	report_id := mux.Vars(r)["id"]
 	var reports []Reports
 	queryReports:= models.Db.Table("report.report_list").
-	Select("report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.report_segment.level", "report.report_segment.note").
+	Select("report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.report_segment.level", "report.report_segment.note", "report.status.name as status").
+	Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
 	Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
 	Where("report.report_list.id = ?", report_id).Scan(&reports)
 
