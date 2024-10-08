981e30415e5f23611aced470a3631cb79701698c
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-08 16:35:58 +0700

    Add Joins to the map.irrigations_segment table. Then, add the center_point_json columns from the map.irrigations_segment table in the Select statement.

--- controllers/reports.go
+++ controllers/reports.go
@@ -23,9 +23,10 @@ func GetReportById(w http.ResponseWriter, r *http.Request){
 	report_id := mux.Vars(r)["id"]
 	var reports []Reports
 	queryReports:= models.Db.Table("report.report_list").
-	Select("report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.report_segment.level", "report.report_segment.note", "report.status.name as status").
+	Select("report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.report_segment.level", "report.report_segment.note", "report.status.name as status", "map.irrigations_segment.center_point_json").
 	Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
 	Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
+	Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
 	Where("report.report_list.id = ?", report_id).Scan(&reports)
 
 	if queryReports.Error != nil {
