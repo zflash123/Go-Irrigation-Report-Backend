352410da87b3658dd3e864752882355c049fdd66
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-08 17:00:25 +0700

    Add Joins to the report.report_photo and file.upload_dump table. Then, add the type columns from the map.irrigations table and add file_url column from the file.upload_dump in the Select statement.

--- controllers/reports.go
+++ controllers/reports.go
@@ -26,9 +26,11 @@ func GetReportById(w http.ResponseWriter, r *http.Request){
 	report_id := mux.Vars(r)["id"]
 	var reports []Reports
 	queryReports:= models.Db.Table("report.report_list").
-	Select("report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.report_segment.level", "report.report_segment.note", "report.status.name as status", "map.irrigations_segment.center_point_json", "map.irrigations.name as irrigation_name").
+	Select("report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.report_segment.level", "report.report_segment.note", "report.status.name as status", "map.irrigations_segment.center_point_json", "map.irrigations.name as irrigation_name", "map.irrigations.type as canal", "file.upload_dump.file_url as image").
 	Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
 	Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
+	Joins("JOIN report.report_photo ON report.report_photo.report_segment_id = report.report_segment.id").
+	Joins("JOIN file.upload_dump ON file.upload_dump.id = report.report_photo.upload_dump_id").
 	Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
 	Joins("JOIN map.irrigations ON map.irrigations.id = map.irrigations_segment.irrigation_id").
 	Where("report.report_list.id = ?", report_id).Scan(&reports)
