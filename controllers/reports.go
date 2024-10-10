ddb952aa08dc1c745f580160b02da77f8f50d746
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-10 13:50:49 +0700

    Convert the year variable data type from int to string and store its value in the strYear variable.

--- controllers/reports.go
+++ controllers/reports.go
@@ -49,4 +49,5 @@ func CreateReport(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	r.ParseForm()
 	year, month, day := time.Now().Date()
+	strYear := fmt.Sprintf("%v", year)
 }
