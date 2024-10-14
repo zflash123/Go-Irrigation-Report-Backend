3a11592361cb51a5b44580c7a26ba1e3451b748f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-14 09:46:37 +0700

    Split the image string until the system gets the image mime type.

--- controllers/upload_image.go
+++ controllers/upload_image.go
@@ -1,5 +1,11 @@
 package controllers
 
+import(
+	"strings"
+)
+
 func UploadImage(image string) error {
-	
+	parts := strings.Split(image, ";")
+	mimePart := strings.Split(parts[0], ":")
+	mime := mimePart[1]
 }
