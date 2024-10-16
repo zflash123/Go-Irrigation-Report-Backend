798b3b74c56b1919a7e9f60d5ca3ea63517b53a3
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-16 09:38:10 +0700

    Get the value of the GCS_BUCKET key from the ".env" file and store that value in the bucket variable.

--- controllers/upload_to_firebase.go
+++ controllers/upload_to_firebase.go
@@ -1,4 +1,11 @@
 package controllers
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
 func UploadToFirebase(imagePath string, objectName string) error {
+	viperEnvConfig()
+	bucket := fmt.Sprintf("%v", viper.Get("GCS_BUCKET"))
 }
