98b83112c7ec3e24cc3604b5577df548e65df5f9
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-16 09:41:06 +0700

    Create a ctx object with an empty context value.

--- controllers/upload_to_firebase.go
+++ controllers/upload_to_firebase.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"fmt"
+	"context"
 	"github.com/spf13/viper"
 )
 
 func UploadToFirebase(imagePath string, objectName string) error {
 	viperEnvConfig()
 	bucket := fmt.Sprintf("%v", viper.Get("GCS_BUCKET"))
+	ctx := context.Background()
 }
