17f9be4199377fc96955e2fcbcd883955ea4a829
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-17 16:39:25 +0700

    Add fileUrl as return data in the UploadToFirebase function. Fill the fileUrl with URL of the image file that will be uploaded.

--- controllers/upload_to_firebase.go
+++ controllers/upload_to_firebase.go
@@ -12,23 +12,24 @@ import (
 	"google.golang.org/api/option"
 )
 
-func UploadToFirebase(imagePath string, objectName string) error {
+func UploadToFirebase(imagePath string, objectName string) (fileUrl string, err error) {
 	viperEnvConfig()
 	bucket := fmt.Sprintf("%v", viper.Get("GCS_BUCKET"))
+	fileUrl = "https://storage.googleapis.com/"+bucket+"/"+objectName
 	ctx := context.Background()
 	wd, _ := os.Getwd()
 	credentialsPath := fmt.Sprintf("%v/firebase-storage-credentials.json", wd)
 	opt := option.WithCredentialsFile(credentialsPath)
 	client, err := storage.NewClient(ctx, opt)
 	if err != nil {
-		return fmt.Errorf("storage.NewClient: %w", err)
+		return "", fmt.Errorf("storage.NewClient: %w", err)
 	}
 	defer client.Close()
 
 	// Open local file.
 	f, err := os.Open(imagePath)
 	if err != nil {
-		return fmt.Errorf("os.Open: %w", err)
+		return "", fmt.Errorf("os.Open: %w", err)
 	}
 	defer f.Close()
 
@@ -41,10 +42,10 @@ func UploadToFirebase(imagePath string, objectName string) error {
 	// Upload an object with storage.Writer.
 	sw := o.NewWriter(ctx)
 	if _, err = io.Copy(sw, f); err != nil {
-		return fmt.Errorf("io.Copy: %w", err)
+		return "", fmt.Errorf("io.Copy: %w", err)
 	}
 	if err := sw.Close(); err != nil {
-		return fmt.Errorf("Writer.Close: %w", err)
+		return "", fmt.Errorf("Writer.Close: %w", err)
 	}
-	return nil
+	return fileUrl, nil
 }
