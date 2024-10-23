e6738bed30bf58b7e5ea7d3839dbea8d79148468
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-10-23 13:47:43 +0700

    Create Segments struct. Create GetSegmentsByUserId function that has a query that gets all segments data reported by the user. Then, return the result in JSON format at "get_segments.go" file.

--- controllers/get_segments.go
+++ controllers/get_segments.go
@@ -15,6 +15,16 @@ type CloseSegments struct {
 	Geojson string    `json:"geojson"`
 }
 
+type Segments struct {
+	SegmentID 					uuid.UUID `json:"id"`
+	IrrigationName			string    `json:"irrigation"`
+	Geojson 						string    `json:"geojson"`
+	Level								string		`json:"level"`
+	Status							string		`json:"status"`
+	Canal								string		`json:"canal"`
+	Image								string		`json:"image"`
+}
+
 func GetCloseSegments(w http.ResponseWriter, r *http.Request) {
 	latitude := r.URL.Query().Get("lat")
 	longitude := r.URL.Query().Get("long")
@@ -33,6 +43,25 @@ func GetCloseSegments(w http.ResponseWriter, r *http.Request) {
             distance;`, longitude, latitude, longitude, latitude).Scan(&closeSegments)
 
 	err := json.NewEncoder(w).Encode(closeSegments)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+}
+
+func GetSegmentsByUserId(w http.ResponseWriter, r *http.Request) {
+	user_id := fmt.Sprintf("%v", r.Context().Value("user_id"))
+
+	var segments []Segments 
+	models.Db.Table("report.report_list").Select("report.status.name as status", "report.report_segment.segment_id", "report.report_segment.level", "map.irrigations_segment.geojson", "map.irrigations.name as irrigation_name", "map.irrigations.type as canal", "file.upload_dump.file_url as image").
+	Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
+	Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
+	Joins("JOIN report.report_photo ON report.report_photo.report_segment_id = report.report_segment.id").
+	Joins("JOIN file.upload_dump ON file.upload_dump.id = report.report_photo.upload_dump_id").
+	Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
+	Joins("JOIN map.irrigations ON map.irrigations.id = map.irrigations_segment.irrigation_id").
+	Where("report.report_list.user_id = ?", user_id).Scan(&segments)
+
+	err := json.NewEncoder(w).Encode(segments)
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
