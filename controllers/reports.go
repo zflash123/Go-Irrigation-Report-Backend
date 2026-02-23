d6e405d7213df31512829ebaa3c745ec2abf8fe5
Zaed Abdullah <z4ed.thalib123@gmail.com>
2026-02-23 22:11:02 +0700

    Add conditional logic to query when there is search or and filter query param in GetReportByUserId

--- controllers/reports.go
+++ controllers/reports.go
@@ -48,15 +48,47 @@ func GetReportById(w http.ResponseWriter, r *http.Request){
 
 func GetReportByUserId(w http.ResponseWriter, r *http.Request){
 	user_id := fmt.Sprintf("%v", r.Context().Value("user_id"))
+	filter := r.URL.Query().Get("filter")
+	search := r.URL.Query().Get("search")
 
 	var reports []Reports
-	queryReports:= models.Db.Table("report.report_list").
-	Select("DISTINCT ON (report.report_list.id) report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.status.name as status", "map.irrigations.name as irrigation_name", "map.irrigations.type as canal").
-	Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
-	Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
-	Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
-	Joins("JOIN map.irrigations ON map.irrigations.id = map.irrigations_segment.irrigation_id").
-	Where("report.report_list.user_id = ?", user_id).Scan(&reports)
+	var queryReports = models.Db
+	if(filter != "" && search != ""){
+		queryReports = models.Db.Table("report.report_list").
+		Select("DISTINCT ON (report.report_list.id) report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.status.name as status", "map.irrigations.name as irrigation_name", "map.irrigations.type as canal").
+		Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
+		Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
+		Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
+		Joins("JOIN map.irrigations ON map.irrigations.id = map.irrigations_segment.irrigation_id").
+		Where("report.report_list.user_id = ? AND report.status.name = ? AND map.irrigations.name = ?", user_id, filter, search).
+		Scan(&reports)
+	} else if filter != ""{
+		queryReports = models.Db.Table("report.report_list").
+		Select("DISTINCT ON (report.report_list.id) report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.status.name as status", "map.irrigations.name as irrigation_name", "map.irrigations.type as canal").
+		Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
+		Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
+		Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
+		Joins("JOIN map.irrigations ON map.irrigations.id = map.irrigations_segment.irrigation_id").
+		Where("report.report_list.user_id = ? AND report.status.name = ?", user_id, filter).
+		Scan(&reports)
+	} else if search != ""{
+		queryReports = models.Db.Table("report.report_list").
+		Select("DISTINCT ON (report.report_list.id) report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.status.name as status", "map.irrigations.name as irrigation_name", "map.irrigations.type as canal").
+		Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
+		Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
+		Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
+		Joins("JOIN map.irrigations ON map.irrigations.id = map.irrigations_segment.irrigation_id").
+		Where("report.report_list.user_id = ? AND map.irrigations.name LIKE %?%", user_id, search).
+		Scan(&reports)
+	} else {
+		queryReports = models.Db.Table("report.report_list").
+		Select("DISTINCT ON (report.report_list.id) report.report_list.id", "report.report_list.created_at", "report.report_list.done_at", "report.status.name as status", "map.irrigations.name as irrigation_name", "map.irrigations.type as canal").
+		Joins("JOIN report.status ON report.status.id = report.report_list.status_id").
+		Joins("JOIN report.report_segment ON report.report_segment.report_id = report.report_list.id").
+		Joins("JOIN map.irrigations_segment ON map.irrigations_segment.id = report.report_segment.segment_id").
+		Joins("JOIN map.irrigations ON map.irrigations.id = map.irrigations_segment.irrigation_id").
+		Where("report.report_list.user_id = ?", user_id).Scan(&reports)
+	}
 
 	if queryReports.Error != nil {
 		fmt.Printf("%v", queryReports.Error)
