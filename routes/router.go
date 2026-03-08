818771c510249e2eb1214a2b21a30cc4f5c09e1c
Zaed Abdullah <z4ed.thalib123@gmail.com>
2026-03-08 16:14:00 +0700

    Fix some API Endpoints naming in router.go

--- routes/router.go
+++ routes/router.go
@@ -26,9 +26,9 @@ func Routes() {
 	user.HandleFunc("/check-valid-cookie", controllers.CheckValidJwt).Methods("GET")
 	user.HandleFunc("/close-segments", controllers.GetCloseSegments).Methods("GET")
 	user.HandleFunc("/user/segments", controllers.GetSegmentsByUserId).Methods("GET")
-	user.HandleFunc("/user/report/{id}", controllers.GetReportById).Methods("GET")
+	user.HandleFunc("/report/{id}", controllers.GetReportById).Methods("GET")
 	user.HandleFunc("/user-reports", controllers.GetReportByUserId).Methods("GET")
-	user.HandleFunc("/user/report", controllers.CreateReport).Methods("POST")
+	user.HandleFunc("/report", controllers.CreateReport).Methods("POST")
 	user.HandleFunc("/user/profile", controllers.GetUserProfile).Methods("GET")
 	user.HandleFunc("/user/profile", controllers.PutUserProfile).Methods("PUT")
 
