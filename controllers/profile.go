d8c6332e72564c43c7b78051f88b70184c19f3b2
Zaed Abdullah <z4ed.thalib123@gmail.com>
2026-03-30 13:43:37 +0700

    Add goroutine after sending the response body in UpdateUserProfile controller

--- controllers/profile.go
+++ controllers/profile.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"go-irrigation-report-backend/models"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/google/uuid"
 )
@@ -75,7 +77,16 @@ func UpdateUserProfile(w http.ResponseWriter, r *http.Request) {
 			if err != nil {
 				fmt.Printf("%v", err)
 			}
+			//Remove the Uploaded Image in this Working Directory outside of main thread for faster response of API
+			return
 		}
+		var res Response
+		res.Message = "Update profile successful"
+		w.WriteHeader(http.StatusOK)
+		err = json.NewEncoder(w).Encode(res)
+		go func(imagePath string) {
+			os.Remove(imagePath)
+		}(imagePath)
 	} else {
 		query := models.Db.Model(&user).Updates(models.User{
 			FirstName: profileFormData.Firstname,
