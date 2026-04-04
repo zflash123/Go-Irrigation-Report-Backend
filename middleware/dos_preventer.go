0068663bdb93b4c42c933c9cb28b92762cf8faa6
Zaed Abdullah <z4ed.thalib123@gmail.com>
2026-04-04 23:08:46 +0700

    Make DoS Preventer works perfectly

--- middleware/dos_preventer.go
+++ middleware/dos_preventer.go
@@ -1,7 +1,8 @@
 package middleware
 
 import (
-	"encoding/json"
+	"bytes"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -13,25 +14,27 @@ func GlobalDosPreventer(next http.Handler) http.Handler {
 	//This middleware only limit the size of the request Body
 	//It is because the request Header size limiter already handled by the net/http by default
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
+		// Get HTTP_REQUEST_LIMIT_IN_MB and convert it from MB to bytes int64
 		var requestSizeLimit string
 		var requestSizeLimitInInt int
-		//Type assertion to change data type from any to int64
+			//Type assertion to change data type from any to int64
 		requestSizeLimit = viper.Get("HTTP_REQUEST_LIMIT_IN_MB").(string)
 		requestSizeLimitInInt, _ = strconv.Atoi(requestSizeLimit)
-		maxBytesSize := int64(requestSizeLimitInInt) << 20
-		//Add HTTP Request Body Size Limiter Handler
-		r.Body = http.MaxBytesReader(w, r.Body, maxBytesSize)
-		err := r.ParseForm()
-		if err != nil{
-			var res Response
-			res.Message = "Your request is limited for DoS Prevention"
-			w.WriteHeader(http.StatusRequestEntityTooLarge)
-			err := json.NewEncoder(w).Encode(res)
-			if err != nil {
-				log.Println("Error when Encode the res to JSON using w Encoder")
-			}
+		requestSizeLimitInBytes := int64(requestSizeLimitInInt) << 20
+		//HTTP Request Body Size Limiter
+		dataRBody, _ := io.ReadAll(r.Body)
+		newRBody := io.LimitReader(bytes.NewBuffer(dataRBody), requestSizeLimitInBytes)
+			//Detect the error of reading above the requestSizeLimitInBytes
+		var err error
+		_, err = io.ReadFull(newRBody, dataRBody)
+		r.Body = io.NopCloser(bytes.NewBuffer(dataRBody))
+		log.Println("Err inside if ct x-www-form= ",err)
+			//Handling if error is true
+		if err == io.ErrUnexpectedEOF{
+			http.Error(w, "Error: DoS Prevented", 413)
+			return
 		}
-		//Return the new Handler
+		// If there is no error then continue the serve of HTTP with r and w
 		next.ServeHTTP(w, r)
 	})
 }
