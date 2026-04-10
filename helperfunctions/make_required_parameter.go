9a2af7224efbe5257d09e4f4ec5689845a415fee
zflash123 <z4ed.thalib123@gmail.com>
2026-04-10 19:45:53 +0700

    Add MakeRequiredParameterType{DataType} instead, so I can improve the performance of Go App

--- helperfunctions/make_required_parameter.go
+++ helperfunctions/make_required_parameter.go
@@ -5,38 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"reflect"
-	"regexp"
 )
 
 type Response struct {
 	Message string
 }
 
-func MakeRequiredParameter(parameter any, parameterName string, w http.ResponseWriter)(isParameterEmpty bool){
-	assertedParameter := reflect.ValueOf(parameter)
-	assertedParameterType := assertedParameter.Type().Name()
-
-	if assertedParameterType == "string"{
-		isParameterEmpty = (assertedParameter.String()=="")
-	} else{
-		isTypeIntOrFloat, _ :=regexp.MatchString("(int.*)|(float.*)", assertedParameterType)
-		if isTypeIntOrFloat {
-			isParameterEmpty = assertedParameter.IsZero()
-		} else {
-			isParameterEmpty = !assertedParameter.IsValid()
-		}
-	}
+func MakeRequiredParameterTypeString(parameter string, parameterName string, w http.ResponseWriter)(isParameterEmpty bool){
+	isParameterEmpty = (parameter=="")
 	if isParameterEmpty{
 		var res Response
 		res.Message = fmt.Sprintf("Parameter %s is required",parameterName)
 		w.WriteHeader(http.StatusBadRequest)
 		err := json.NewEncoder(w).Encode(res)
 		if err != nil {
-			log.Printf("Error when checking the required Parameters")
+			log.Println("Error when checking the required Parameters")
 		}
-		return isParameterEmpty
-	} else{
-		return false
 	}
+	return isParameterEmpty
 }
