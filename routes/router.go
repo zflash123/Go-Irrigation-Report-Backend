e395a9e94b46a17fd30f990f1a4d189641881839
zflash123 <z4ed.thalib123@gmail.com>
2026-04-10 17:47:17 +0700

    Make http listen to "0.0.0.0" instead, for giving more flexibility of using the code for development and production use(without change the code again and again)

--- routes/router.go
+++ routes/router.go
@@ -34,5 +34,5 @@ func Routes() {
 
 	corsHandler := config.CorsObject.Handler(r)
 	fmt.Println("HTTP server run on port 8080")
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", middleware.GlobalDosPreventer(corsHandler)))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", middleware.GlobalDosPreventer(corsHandler)))
 }
