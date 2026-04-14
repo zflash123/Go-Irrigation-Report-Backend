711dd1ce6fbd537ef75c19e097313858dd2710a5
zflash123 <z4ed.thalib123@gmail.com>
2026-04-14 13:28:10 +0700

    Remove 0.0.0.0 in http.ListenAndServe

--- routes/router.go
+++ routes/router.go
@@ -36,5 +36,5 @@ func Routes() {
 
 	corsHandler := config.CorsObject.Handler(r)
 	fmt.Println("HTTP server run on port 8080")
-	log.Println(http.ListenAndServe("0.0.0.0:8080", middleware.GlobalDosPreventer(corsHandler)))
+	log.Println(http.ListenAndServe(":8080", middleware.GlobalDosPreventer(corsHandler)))
 }
